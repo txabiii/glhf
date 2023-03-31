d3299e51281a3608eafde5a18eaa458e91923452
txabiii <txabiguerrero2000@gmail.com>
2023-03-31 14:00:55 +0800

    Vercel debugging

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,19 @@
 "use strict"
 require('dotenv').config();
 
+const app = express();
+
 const express = require("express");
 const cors = require('cors');
 
-const app = express();
-
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true});
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+  console.log("Mongoose database connection established succesfully!");
+})
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,6 @@ const app = require("./app");
 
 const port = process.env.PORT || 5000;
 
-const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, { useNewUrlParser: true});
-const connection = mongoose.connection;
-connection.once('open', ()=>{
-  console.log("Mongoose database connection established succesfully!");
-})
-
 app.use("/", routes);
 
 app.listen(port, ()=>{
