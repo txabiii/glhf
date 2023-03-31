d84e0649a130304b3aceaa1e23ff7d91b3927e95
txabiii <txabiguerrero2000@gmail.com>
2023-03-31 21:53:29 +0800

    Update _app.tsx

--- glhf/src/pages/_app.tsx
+++ glhf/src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
