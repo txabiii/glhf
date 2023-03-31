abb89984c876be246e829542241206db8675f36a
txabiii <txabiguerrero2000@gmail.com>
2023-03-31 16:42:22 +0800

    Vercel debugging 4:42pm

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
 "use strict"
 require('dotenv').config();
 
-const app = express();
-
 const express = require("express");
 const cors = require('cors');
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
