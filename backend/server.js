ea031c418d9519497ac5e220ba2044f9fd4b7823
txabiii <txabiguerrero2000@gmail.com>
2023-03-31 12:17:21 +0800

    Vercel debugging

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const app = require("./app");
 
 const port = process.env.PORT || 5000;
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.MONGODB_URL;
 mongoose.connect(uri, { useNewUrlParser: true});
 const connection = mongoose.connection;
 connection.once('open', ()=>{
